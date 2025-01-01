da281c77b072cc8104fce859d5be1466d1303638
JanSharp <steinmeister36@gmail.com>
2025-01-01 20:16:36 +0100

    Fix attributes not being public

--- Runtime/EntitySystemAttributes.cs
+++ Runtime/EntitySystemAttributes.cs
@@ -2,14 +2,14 @@
 namespace JanSharp
 {
     [System.AttributeUsage(System.AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
-    sealed class EntityExtensionInputActionAttribute : System.Attribute
+    public class EntityExtensionInputActionAttribute : System.Attribute
     {
         public EntityExtensionInputActionAttribute()
         { }
     }
 
     [System.AttributeUsage(System.AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
-    sealed class AssociatedEntityExtensionDataAttribute : System.Attribute
+    public class AssociatedEntityExtensionDataAttribute : System.Attribute
     {
         readonly System.Type associatedDataType;
         public System.Type AssociatedDataType => associatedDataType;
