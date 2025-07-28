ac1f7e18e8bd276a3a283b5dfbc67c740ecd9203
JanSharp <steinmeister36@gmail.com>
2025-07-29 00:56:45 +0200

    Populate EntitySystem prototypes list on build

--- Editor/EntitySystemEditor.cs
+++ Editor/EntitySystemEditor.cs
@@ -5,6 +5,38 @@ using UnityEngine;
 
 namespace JanSharp
 {
+    [InitializeOnLoad]
+    public static class EntitySystemOnBuild
+    {
+        private static List<EntityPrototype> prototypes;
+
+        static EntitySystemOnBuild()
+        {
+            OnBuildUtil.RegisterTypeCumulative<EntityPrototype>(OnPrototypesBuild, order: -1);
+            OnBuildUtil.RegisterType<EntitySystem>(OnBuild);
+        }
+
+        private static bool OnPrototypesBuild(IEnumerable<EntityPrototype> prototypes)
+        {
+            EntitySystemOnBuild.prototypes = prototypes.ToList();
+            return true;
+        }
+
+        private static bool OnBuild(EntitySystem entitySystem)
+        {
+            SerializedObject so = new SerializedObject(entitySystem);
+
+            EditorUtil.SetArrayProperty(
+                so.FindProperty("entityPrototypes"),
+                prototypes,
+                (p, v) => p.objectReferenceValue = v);
+            prototypes = null; // Cleanup.
+
+            so.ApplyModifiedProperties();
+            return true;
+        }
+    }
+
     public static class EntitySystemEditorUtil
     {
         private static Dictionary<GameObject, EntityPrototypeDefinition> prefabToPrototypeDefLut = new();
