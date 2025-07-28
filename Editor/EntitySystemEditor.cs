4661a2bf36d2bf167d3ac47538e4758a4ab890a2
JanSharp <steinmeister36@gmail.com>
2025-07-29 01:49:41 +0200

    Split EntitySystemEditor file into 2

--- Editor/EntitySystemEditor.cs
+++ Editor/EntitySystemEditor.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
-using UnityEngine;
 
 namespace JanSharp
 {
@@ -34,32 +33,6 @@ namespace JanSharp
 
             so.ApplyModifiedProperties();
             return true;
-        }
-    }
-
-    public static class EntitySystemEditorUtil
-    {
-        private static Dictionary<GameObject, EntityPrototypeDefinition> prefabToPrototypeDefLut = new();
-
-        public static bool TryGetPrototypeDefinition(GameObject prefabAsset, out EntityPrototypeDefinition prototypeDefinition)
-        {
-            if (prefabToPrototypeDefLut.TryGetValue(prefabAsset, out prototypeDefinition)
-                && prototypeDefinition != null && prototypeDefinition.entityPrefab == prefabAsset)
-            {
-                return true;
-            }
-            FindAllPrototypeDefinitions();
-            return prefabToPrototypeDefLut.TryGetValue(prefabAsset, out prototypeDefinition);
-        }
-
-        public static void FindAllPrototypeDefinitions()
-        {
-            // TODO: Somehow error when multiple prototypes are using the same prefab.
-            string[] guids = AssetDatabase.FindAssets("t:EntityPrototypeDefinition");
-            prefabToPrototypeDefLut = guids
-                .Select(guid => AssetDatabase.LoadAssetAtPath<EntityPrototypeDefinition>(AssetDatabase.GUIDToAssetPath(guid)))
-                .Where(p => p.entityPrefab != null)
-                .ToDictionary(p => p.entityPrefab, p => p);
         }
     }
 }
