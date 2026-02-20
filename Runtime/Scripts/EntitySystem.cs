JanSharp/VRCEntitySystem

commit 4d343b9d947edf167df430999665d02a48b15226

Extend availability of import related remapping

Runtime/Scripts/EntitySystem.cs
@@ -792,7 +792,7 @@ namespace JanSharp
             return null;
         }
 
-        [LockstepEvent(LockstepEventType.OnImportFinished)]
+        [LockstepEvent(LockstepEventType.OnImportFinished, Order = 10000)]
         public void OnImportFinished()
         {
 #if ENTITY_SYSTEM_DEBUG

commit 64a4f4ca1acc0a4b3b6fd92d613a95c2831598de

Fix exception when not part of imported data
Introduced in 4d343b9d947edf167df430999665d02a48b15226

Runtime/Scripts/EntitySystem.cs
@@ -882,6 +882,8 @@ namespace JanSharp
 #if ENTITY_SYSTEM_DEBUG
             Debug.Log($"[EntitySystemDebug] EntitySystem  OnImportFinished");
 #endif
+            if (!IsPartOfCurrentImport)
+                return;
             // Kept these alive for longer so game states importing after the EntitySystem could still use them.
             DataList list = importedPrototypeMetadataById.GetValues();
             for (int i = 0; i < list.Count; i++)