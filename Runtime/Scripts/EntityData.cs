JanSharp/VRCEntitySystem

commit ca2af54dab1d448cd5cc230d0abdacd0f8c6cb20

Make exports include up to date transform values
Specifically for entity data with desynced transform values, aka those
  with custom transform controllers

Runtime/Scripts/EntityData.cs
@@ -426,15 +426,33 @@ namespace JanSharp
 #if ENTITY_SYSTEM_DEBUG
             Debug.Log($"[EntitySystemDebug] EntityData  SerializeTransformValues");
 #endif
-            lockstep.WriteVector3(position);
-            lockstep.WriteQuaternion(rotation);
-            lockstep.WriteVector3(scale);
-            if (!noTransformSync)
+            if (!noTransformSync) // For both serialization and exports.
+            {
+                lockstep.WriteVector3(position);
+                lockstep.WriteQuaternion(rotation);
+                lockstep.WriteVector3(scale);
                 return;
+            }
             if (!isExport)
+            {
                 lockstep.WriteSmallUInt(lastKnownTransformStateTick);
-            else
-                lockstep.WriteSmallUInt(lockstep.CurrentTick - lastKnownTransformStateTick);
+                return;
+            }
+            if (entity == null)
+            {
+                lockstep.WriteVector3(position);
+                lockstep.WriteQuaternion(rotation);
+                lockstep.WriteVector3(scale);
+                lockstep.WriteSmallUInt(lockstep.CurrentTick - lastKnownTransformStateTick); // ticksAgo
+                return;
+            }
+            // Exports can actually use non game state safe data.
+            // Imports are going to turn that data into game state safe data anyway.
+            Transform entityTransform = entity.transform;
+            lockstep.WriteVector3(entityTransform.position);
+            lockstep.WriteQuaternion(entityTransform.rotation);
+            lockstep.WriteVector3(entityTransform.localScale);
+            lockstep.WriteSmallUInt(0u); // ticksAgo
         }
 
         private void DeserializeTransformValues(bool isImport)

commit ca31381f5f446eaf1c5ef2f6565140dd891f6332

Fix noTransformSync EntityData LJ serialization
Regression from ca2af54dab1d448cd5cc230d0abdacd0f8c6cb20

Runtime/Scripts/EntityData.cs
@@ -426,33 +426,28 @@ namespace JanSharp
 #if ENTITY_SYSTEM_DEBUG
             Debug.Log($"[EntitySystemDebug] EntityData  SerializeTransformValues");
 #endif
+            if (isExport && noTransformSync && entity != null)
+            {
+                // Exports can actually use non game state safe data.
+                // Imports are going to turn that data into game state safe data anyway.
+                Transform entityTransform = entity.transform;
+                lockstep.WriteVector3(entityTransform.position);
+                lockstep.WriteQuaternion(entityTransform.rotation);
+                lockstep.WriteVector3(entityTransform.localScale);
+                lockstep.WriteSmallUInt(0u); // ticksAgo
+                return;
+            }
+
+            lockstep.WriteVector3(position);
+            lockstep.WriteQuaternion(rotation);
+            lockstep.WriteVector3(scale);
+
             if (!noTransformSync) // For both serialization and exports.
-            {
-                lockstep.WriteVector3(position);
-                lockstep.WriteQuaternion(rotation);
-                lockstep.WriteVector3(scale);
-                return;
-            }
-            if (!isExport)
-            {
+            { } // Weird empty block just to match the conditions in the deserialize function exactly.
+            else if (!isExport)
                 lockstep.WriteSmallUInt(lastKnownTransformStateTick);
-                return;
-            }
-            if (entity == null)
-            {
-                lockstep.WriteVector3(position);
-                lockstep.WriteQuaternion(rotation);
-                lockstep.WriteVector3(scale);
+            else
                 lockstep.WriteSmallUInt(lockstep.CurrentTick - lastKnownTransformStateTick); // ticksAgo
-                return;
-            }
-            // Exports can actually use non game state safe data.
-            // Imports are going to turn that data into game state safe data anyway.
-            Transform entityTransform = entity.transform;
-            lockstep.WriteVector3(entityTransform.position);
-            lockstep.WriteQuaternion(entityTransform.rotation);
-            lockstep.WriteVector3(entityTransform.localScale);
-            lockstep.WriteSmallUInt(0u); // ticksAgo
         }
 
         private void DeserializeTransformValues(bool isImport)
@@ -463,6 +458,7 @@ namespace JanSharp
             position = lockstep.ReadVector3();
             rotation = lockstep.ReadQuaternion();
             scale = lockstep.ReadVector3();
+
             if (!noTransformSync)
                 lastKnownTransformStateTick = 0u;
             else if (!isImport)